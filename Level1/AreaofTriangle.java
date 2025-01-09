804d13839751a72955d3e006655b282fe49ba053
Nitish singh <nitishkumarsingh877@gmail.com>
2025-01-09 15:35:24 +0530

    AreaofTriangle added

---
+++ Level1/AreaofTriangle.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class AreaofTriangle {
+	 public static void main(String[] args) {
+
+        // Create a input object for input
+        Scanner input = new Scanner(System.in);
+
+        // Taking user input for base and height
+
+        System.out.print("Enter the base:  ");
+        double base = input.nextDouble();
+
+        System.out.print("Enter the height: ");
+        double height = input.nextDouble();
+
+        // Performing  operations
+       	double area = 0.5*base*height;
+
+        // Printing the results
+        System.out.println("Your Height in cm is " + height + " and base is "+
+ base + "and area is"+ area);
+        
+        // Close the scanner
+        input.close();
+    }
+}
