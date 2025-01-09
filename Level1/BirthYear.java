f7c7c41a6c16b1fbe5444e845fc66aacb2df80f1
Nitish singh <nitishkumarsingh877@gmail.com>
2025-01-09 13:55:22 +0530

    Completed first 5 ques.

---
+++ Level1/BirthYear.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+//taking class name as DisplayAge as result is AGE(BirthYeara)
+
+public class birthYear {
+	
+	public static void main(String[] args) {
+		 int birthyear = 2000;
+	         int currentyear = 2024;
+        	 int age = currentyear-birthyear;
+
+          	System.out.println("Harry's age in 2024 is " + age);
+}
+}
