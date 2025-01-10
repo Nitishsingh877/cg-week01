24357f36771724745804fa266736e1915bc6586c
Nitish singh <nitishkumarsingh877@gmail.com>
2025-01-10 10:55:12 +0530

    added while loop for sum

---
+++ Day02/Level01/SumTillZero.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class SumTillZero {
+    public static void main(String[] args) {
+
+        // Create a Scanner object for user input
+        Scanner input = new Scanner(System.in);
+
+        // Take user input for the starting value of the Summ
+
+	double total = 0.0;
+	double number;
+
+	System.out.println("Enter numbers to sum them up. Enter 0 to stop.");
+	System.out.println("Enter the  number for the Sum: ");
+	number = input.nextDouble();
+
+	        
+
+        // taking while loop
+	while(number !=0) {
+	total += number;
+	System.out.println("enter the number");
+	double temp = input.nextDouble();
+	number = temp; 
+	}
+	System.out.println("Sum of entered numbers: " + total);
+	
+
+	// Close the scanner
+        input.close();
+    }
+}
