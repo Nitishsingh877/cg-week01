c25ecae021c0d8cd82ed902e895a662fe1dba787
Nitish singh <nitishkumarsingh877@gmail.com>
2025-01-15 18:34:19 +0530

    added string in 1D

--- String/Level02/SplitText.java
+++ String/Level02/SplitText.java
@@ -2,46 +2,53 @@
 import java.util.Scanner;
 
 public class SplitText {
-    public static int findLength(String s) {
-        //count the strings
-        int count = 0;
-        try {
-            while (true) {
-                s.charAt(count); // Access characters until IndexOutOfBoundsException
-                count++;
-            }
-        } catch (IndexOutOfBoundsException e) {
-            // Exception caught when index exceeds the string length
+    public static String[] splitString(String s){
+        int size = 1;
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == ' ')
+                size++;
         }
-        return count;
+
+        String[] answer = new String[size];
+        int id = 0;
+        String temp = "";
+
+        for (int i = 0; i < s.length(); i++) {
+            if(s.charAt(i) == ' ') {
+                answer[id++] = temp;
+                temp = "";
+            }
+            else{
+                temp+=s.charAt(i);
+            }
+        }
+        answer[id] = temp;
+        return answer;
     }
-    //method to split into words
-    public static String[] customSplit(String text) {
-        int length = findLength(text);
-
-        // Count the number of words and store space indexes
-        int wordCount = 1; // At least one word is present
-        for (int i = 0; i < length; i++) {
-            if (text.charAt(i) == ' ') {
-                wordCount++;
-            }
+    public static boolean compare(String[] arr1 , String[] arr2){
+        if(arr1.length != arr2.length) {
+            return false;
         }
-        // Extract words using space indexes
-        String[] words = new String[wordCount];
-        int wordStart = 0, wordIndex = 0;
-        for (int i = 0; i < length; i++) {
-            if (text.charAt(i) == ' ') {
-                words[wordIndex] = text.substring(wordStart, i);
-                wordIndex++;
-                wordStart = i + 1;
-            }
+        for (int i = 0; i < arr1.length; i++) {
+            if(!arr1[i].equals(arr2[i]))
+                return false;
+
         }
+        return true;
+    }
+    public static void main(String[] args){
+        // Created a scanner for inputs
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a sentence");
+        String s = sc.nextLine();
+        // calling use defined split method
+        String[] splited1 = splitString(s);
+        String[] splited2 = s.split(" ");
 
-
-
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-        String s = input.nextLine();
+        if(compare(splited2,splited1))
+            System.out.println("Both are equal");
+        else
+            System.out.println("Both are different;");
 
     }
 }
